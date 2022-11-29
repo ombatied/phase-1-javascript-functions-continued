4350dd938b93abc3010b2e394852be5371a7f2ed
ombatied <ombatied@gmail.com>
2022-11-29 21:36:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+let activity = "roller-skate";
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+let mondayWork = function(input="go to the office") {
+    return `This Monday, I will ${input}.`;
+}
+function wrapAdjective(star = "*") {
+    
+    return function(single = "a hard worker"){
+        return `You are ${star}${single}${star}!`;
+    }
+}
